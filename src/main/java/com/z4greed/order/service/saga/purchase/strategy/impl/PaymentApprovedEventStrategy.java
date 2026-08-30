2a850f827041b7aed549b000eb7b970a995af736
cvaldiviape <cvaldiviape@gmail.com>
2026-08-29 20:39:35 -0500

    refactor(saga): remove unused order confirmed event publication

--- src/main/java/com/z4greed/order/service/saga/purchase/strategy/impl/PaymentApprovedEventStrategy.java
+++ src/main/java/com/z4greed/order/service/saga/purchase/strategy/impl/PaymentApprovedEventStrategy.java
@@ -1,32 +1,21 @@
 package com.z4greed.order.service.saga.purchase.strategy.impl;
 
 import com.z4greed.order.dto.PurchaseSagaContextDto;
-import com.z4greed.order.entity.OrderEntity;
 import com.z4greed.order.enums.EventTypeEnum;
 import com.z4greed.order.enums.OrderStatusEnum;
 import com.z4greed.order.enums.SagaStatusEnum;
-import com.z4greed.order.kafka.factory.OrderEventFactory;
-import com.z4greed.order.kafka.event.EventEnvelopeDto;
-import com.z4greed.order.kafka.producer.OrderEventProducer;
 import com.z4greed.order.service.saga.purchase.strategy.PurchaseSagaEventStrategy;
 import com.z4greed.order.service.saga.purchase.state.PurchaseSagaStateManager;
-import java.util.Map;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentApprovedEventStrategy implements PurchaseSagaEventStrategy {
   private final PurchaseSagaStateManager purchaseSagaStateManager;
-  private final OrderEventFactory orderEventFactory;
-  private final OrderEventProducer orderEventProducer;
 
   public PaymentApprovedEventStrategy(
-      PurchaseSagaStateManager purchaseSagaStateManager,
-      OrderEventFactory orderEventFactory,
-      OrderEventProducer orderEventProducer
+      PurchaseSagaStateManager purchaseSagaStateManager
   ) {
     this.purchaseSagaStateManager = purchaseSagaStateManager;
-    this.orderEventFactory = orderEventFactory;
-    this.orderEventProducer = orderEventProducer;
   }
 
   @Override
@@ -37,14 +26,6 @@ public class PaymentApprovedEventStrategy implements PurchaseSagaEventStrategy {
   @Override
   public void execute(PurchaseSagaContextDto purchaseSagaContextDto) {
     this.purchaseSagaStateManager.update(purchaseSagaContextDto, OrderStatusEnum.CONFIRMED, SagaStatusEnum.COMPLETED, EventTypeEnum.PAYMENT_APPROVED, null);
-
-    OrderEntity orderEntity = purchaseSagaContextDto.orderEntity();
-    String causationId = purchaseSagaContextDto.sourceEvent().eventId();
-    Map<String, Object> mapPayload = Map.of();
-
-    EventEnvelopeDto eventEnvelopeDto = this.orderEventFactory.build(EventTypeEnum.ORDER_CONFIRMED, orderEntity, causationId, mapPayload);
-
-    this.orderEventProducer.publish("orders-events-topic", eventEnvelopeDto);
   }
 
 }

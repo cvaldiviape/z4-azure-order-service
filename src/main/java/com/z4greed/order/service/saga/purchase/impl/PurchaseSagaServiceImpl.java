4d089e4fd571cf45777a666f85fc6dc214d84fae
cvaldiviape <cvaldiviape@gmail.com>
2026-08-30 18:32:57 -0500

    feat(observability): include exception details in saga event logs

--- src/main/java/com/z4greed/order/service/saga/purchase/impl/PurchaseSagaServiceImpl.java
+++ src/main/java/com/z4greed/order/service/saga/purchase/impl/PurchaseSagaServiceImpl.java
@@ -65,7 +65,7 @@ public class PurchaseSagaServiceImpl implements PurchaseSagaService {
     try {
       this.processEvent(eventEnvelopeDto);
     } catch (RuntimeException exception) {
-      log.error("action=event_processing_failed eventType={} eventId={} correlationId={} orderId={}", eventEnvelopeDto.eventType(), eventEnvelopeDto.eventId(), eventEnvelopeDto.correlationId(), eventEnvelopeDto.aggregateId(), exception);
+      log.error("action=event_processing_failed eventType={} eventId={} correlationId={} orderId={} exceptionType={} errorMessage=\"{}\"", eventEnvelopeDto.eventType(), eventEnvelopeDto.eventId(), eventEnvelopeDto.correlationId(), eventEnvelopeDto.aggregateId(), exception.getClass().getSimpleName(), exception.getMessage(), exception);
       throw exception;
     }
   }
